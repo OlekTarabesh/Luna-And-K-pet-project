6c39f179623a4deea45406e7f64d8d225dc69aa7
Alex14121 <oleksandr.tarabesh@gmail.com>
2023-02-26 13:57:20 +0200

    made 'Our philosophy' section

--- src/components/main-page-component/main-page-component.jsx
+++ src/components/main-page-component/main-page-component.jsx
@@ -1,7 +1,8 @@
 import { Fragment } from 'react';
 import Header from '../header/header.component';
-import SecondSlider from '../SecondSlider/second-slider';
+// import SecondSlider from '../SecondSlider/second-slider';
 import { Carousel } from '../SecondSlider/second-slider';
+import Philosophy from '../our-philosophy.component/our-philosophy';
 // import Slider from '../slider-component/slider.component.jsx';
 // import styled from './main-page.module.css';
 
@@ -12,6 +13,7 @@ const MainPage = () => {
         <Fragment>
             <Header />
             <Carousel />
+            <Philosophy />
             {/* <Slider /> */}
         </Fragment>
     )
---
+++ src/components/our-philosophy.component/our-philosophy.jsx
@@ -0,0 +1,50 @@
+import { StarIcon } from '../../icons/star-1-icon';
+import { ellipses } from '../../icons/ellipses/ellipse.data';
+import styled from './our-philosophy.module.css';
+
+
+
+const Philosophy = () => {
+    
+    
+    const { ellipse } = ellipses;
+
+    return(
+        <div className={styled.philosophyWrapper}>
+            <div className={styled.title}>our philosophy</div>
+            <p className={styled.philosophyText}>
+                Our main slogan is ecological, beautiful and strong. Tableware made exclusively by natural materials, which  strong and long-term service. 
+            <br />
+            <br />
+                However, the dishes look incredibly elegant and elegant, necessarily fit into your interior. Our mission is to balance comfort and beauty.
+            </p>
+            <span className={styled.starSvg}><StarIcon /></span>
+            <div className="images">
+                <img 
+                    src={require('../../assets/philosophy/wooden-cutlery.png')} 
+                    alt="alt" 
+                    className={styled.woodenCutleryImg}/>
+                <img 
+                    src={require('../../assets/philosophy/cup.png')} 
+                    alt="alt"
+                    className={styled.cupImg} />
+                <div className={styled.ellipsesContainer}>
+                    {ellipse.map((el) => {
+                    
+                        return (
+                            <>
+                                <h2 className={styled.ellipseText}>Explore further</h2>
+                                <span className={styled.ellipsesImgs}>
+                                    {el.icon}
+                                </span>
+                            </>
+                        )
+                    
+                    })}
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Philosophy;
