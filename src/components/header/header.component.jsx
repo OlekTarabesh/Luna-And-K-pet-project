15757432af5b630c13aa250738564a965a0cfd64
Alex14121 <oleksandr.tarabesh@gmail.com>
2023-03-14 17:31:09 +0200

    made responsive title

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,6 +1,7 @@
 // import { Link } from "react-router-dom";
 import headerImage1 from '../../images/header-images/headerImg.png';
 import headerImage2 from '../../images/header-images/headerImg2.png';
+import ellipseButton from '../../images/header-images/ellipse-button.png';
 import { StarIcon } from "../../icons/star-1-icon.jsx";
 import { ellipses } from '../../icons/ellipses/ellipse.data.js';
 import styled from './header.module.css';
@@ -23,16 +24,18 @@ const Header = () => {
                             <img src={headerImage2} alt='alt' className={styled.headerImage3} />
                         </div>
                 <div className={styled.headerTitleWrapper}>
-                    {/* <h2 className={styled.headerTitle}>Elegant tableware collection for your cosiness</h2> */}
-                    {/* <span className={styled.headerEllipseContainer}>
-                        {ellipse.map((i) => {
+                    <h2 className={styled.headerTitle}>Elegant tableware collection for your cosiness</h2>
+                </div>
+                    <div className={styled.headerEllipseContainer}>
+                        <img src={ellipseButton} alt="alt" className={styled.headerEllipse} />
+                        <h2 className={styled.headerEllipseText}>Go to the catalog</h2>
+                        {/* {ellipse.map((i) => {
                             return (
                                 <span key={Math.random()} className={styled.headerEllipse}>{i.icon}</span>
                                 )     
                             })}
-                            <span className={styled.ellipseText}>Go to the catalog</span>
-                        </span> */}
-                </div>
+                            <span className={styled.ellipseText}>Go to the catalog</span> */}
+                    </div>
             </div>
         </>
     )
--- src/icons/ellipses/ellipse.data.js
+++ src/icons/ellipses/ellipse.data.js
@@ -1,6 +1,6 @@
-import { Ellipse1 } from './Ellipse1.jsx';
-import { Ellipse2 } from './Ellipse2.jsx';
-import { Ellipse3 } from './Ellipse3.jsx';
+// import { Ellipse1 } from './Ellipse1.jsx';
+// import { Ellipse2 } from './Ellipse2.jsx';
+// import { Ellipse3 } from './Ellipse3.jsx';
 import { WhiteEllipse1 } from './WhiteEllipse/WhiteEllipse1.jsx';
 import { WhiteEllipse2 } from './WhiteEllipse/WhiteEllipse2.jsx';
 import { WhiteEllipse3 } from './WhiteEllipse/WhiteEllipse3.jsx';
@@ -8,15 +8,15 @@ import { WhiteEllipse3 } from './WhiteEllipse/WhiteEllipse3.jsx';
 export const ellipses = {
     ellipse:[
         {
-            icon: <Ellipse1 />,
+            // icon: <Ellipse1 />,
             icon2: <WhiteEllipse1 />
         },
         {
-            icon: <Ellipse2 />,
+            // icon: <Ellipse2 />,
             icon2: <WhiteEllipse2 />
         },
         {
-            icon: <Ellipse3 />,
+            // icon: <Ellipse3 />,
             icon2: <WhiteEllipse3 />
         },
     ]
