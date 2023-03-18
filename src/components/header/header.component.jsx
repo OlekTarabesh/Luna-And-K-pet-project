2d819f9a102660497862919696501179342b03f0
Alex14121 <oleksandr.tarabesh@gmail.com>
2023-03-18 12:35:36 +0200

    rewrote responsive layout for header and did some change with burger

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,38 +1,45 @@
 // import { Link } from "react-router-dom";
 import headerImage1 from '../../images/header-images/headerImg.png';
 import headerImage2 from '../../images/header-images/headerImg2.png';
-import { StarIcon } from "../../icons/star-1-icon.jsx";
-import { ellipses } from '../../icons/ellipses/ellipse.data.js';
+import ellipseBtn from '../../icons/ellipses/headerEllipseBtn.png';
+// import { EllipseBtn } from '../../icons/ellipses/headerEllipseBtn';
+// import { StarIcon } from "../../icons/star-1-icon.jsx";
 import styled from './header.module.css';
 
 const Header = () => {
-    const { ellipse } = ellipses;
     return (
         <>
-            <div className={styled.headerWrapper}>
-                <div className={styled.headerContainer}>
-                    <span className={styled.starSvg}>
-                        <StarIcon />
-                    </span>
-                </div>
-                
-                <div className={styled.headerTitleWrapper}>
-                    <h2 className={styled.headerTitle}>Elegant tableware collection for your cosiness</h2>
-                
-                    <img src={headerImage1} alt='alt' className={styled.headerImage1} />
-                    <img src={headerImage2} alt='alt' className={styled.headerImage3} />
-                    <span className={styled.headerEllipseContainer}>
-                        {ellipse.map((i) => {
-                            return (
-                                <span key={Math.random()} className={styled.headerEllipse}>{i.icon}</span>
-                            )     
-                    })}
-                        <span className={styled.ellipseText}>Go to the catalog</span>
-                    </span>
-                </div>
-            </div>
+            <section className={styled.wrapper}>
+                <header className={styled.container}>
+                    <div className={styled.titleAndBtnBlock}>
+                        <h1 className={styled.title}>Elegant tableware collection for your cosiness</h1>
+                        <button className={styled.ellipseBtn}>
+                            <img src={ellipseBtn} alt="alt" className={styled.ellipseBtnImage}/>
+                        </button>
+                    </div>
+                    <div className={styled.mainImageContainer}>
+                        <img src={headerImage1} alt='alt' className={styled.mainImage} />
+                    </div>  
+                    <div className={styled.sideImageContainer}>
+                        <img src={headerImage2} alt='alt' className={styled.sideImage} />
+                    </div>
+                </header>
+            </section>
         </>
     )
 }
 
 export default Header;
+
+
+
+
+{/* <div className={styled.sideImageContainer}> */}
+{/* </div> */}
+
+
+{/* <div className={styled.headerContainer}>
+<span className={styled.starSvg}>
+        <StarIcon />
+        </span>
+    </div> */}
